c2fc61ac11805bf950364b91ad788a50756521b1
aryamanchandra <aryamanhere@gmail.com>
2024-04-23 12:41:06 +0530

    backend connect

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost/book-review-app', {
+mongoose.connect('mongodb://127.0.0.1:27017/book-review-app', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
